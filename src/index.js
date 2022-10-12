9a0d1610c8d63a498a9621773d3990364c7b833c
Edward-Precious Omegbu <eomegbu@gmail.com>
2022-10-12 16:03:20 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css'
+
+const Header = () => {
+  return (
+    <div className="header py-5">
+      <h1 className="text-5xl font-bold text-center mb-4">30 Days Of React</h1>
+      <h2 className="text-3xl font-semibold text-center">Number Generator</h2>
+    </div>
+  )
+}
+
+const Box = ({ array }) => {
+  let classStyles = "w-full aspect-square flex items-center justify-center " + array[1]
+  let Styles = {
+    borderRadius: "5px"
+  }
+  return (
+    <div className={ classStyles } style={ Styles }>
+      <p className="text-xl font-bold text-white">{ array[0] }</p>
+    </div>
+  )
+}
+
+let numArr = Array(32).fill(0).map((item, idx) => {
+  if ([2,3,5,7,11,13,17,19,23,29,31].includes(idx)) return [idx, 'bg-red-500']
+  if (idx % 2 === 0) return [idx, 'bg-green-500']
+  else return [idx, 'bg-yellow-500']
+})
+console.log(numArr)
+
+const BoxSet = ({ array }) => {
+  let boxset = array.map(item => <Box key = { item[0] } array={item} />)
+  return (
+    <div className="boxset grid grid-cols-8 gap-[1px] w-full">
+      { boxset }
+    </div>
+  )
+}
+
+const Main = () => {
+  return (
+    <div className="main w-full mx-auto mt-4">
+      <BoxSet array = {numArr} />
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="body bg-white font-mono min-h-screen px-48">
+      <Header />
+      <Main />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +62,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
